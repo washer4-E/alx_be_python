6935f310dd7e094d1bdb99fc849d7e137e9ff521
ACHRAF <achrafachmar2004@gmail.com>
2025-09-20 15:43:10 +0100

    Add: personal finance calculator with projected annual savings

---
+++ python_introduction/finance_calculator.py
@@ -0,0 +1,10 @@
+ncome = float(input("Enter your monthly income: "))
+expenses = float(input("Enter your total monthly expenses: "))
+
+monthly_savings = ncome - expenses
+annual_savings = monthly_savings * 12
+projected_savings = annual_savings + (annual_savings * 0.05)
+
+print(f"Your monthly savings are ${monthly_savings}.")
+print(f"Projected savings after one year, with interest, is: ${projected_savings}.")
+

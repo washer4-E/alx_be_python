efa47b834e11178965ba6e71aef8e8bbc4289f35
ACHRAF <achrafachmar2004@gmail.com>
2025-09-20 05:30:31 +0100

    Add rectangle area calculator script

---
+++ python_introduction/rectangle_area.py
@@ -0,0 +1,6 @@
+length = 10
+width = 5
+
+Area = length * width
+
+print(f"the area is { Area }")
--- simple_interest.py
+++
@@ -1,7 +0,0 @@
-principal = 1000
-rate = 0.05
-time = 3
-
-interest = principal * rate * time
-
-print(f"the simple interest is {interest}")

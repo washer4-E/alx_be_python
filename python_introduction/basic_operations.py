f33a303c36b83fd64b86874463de2ce12f417827
ACHRAF <achrafachmar2004@gmail.com>
2025-09-20 02:06:48 +0100

    Add: basic arithmetic operations script

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,12 @@
+number1 = 10
+number2 = 5
+
+
+sum_result = number1 + number2
+sub_result = number1 - number2
+mul_result = number1 * number2
+
+print(f"Addition of {number1} and {number2} is {sum_result}")
+print(f"Subtraction of {number1} and {number2} is {sub_result}")
+print(f"Multiplication of {number1} and {number2} is {mul_result}")
+
